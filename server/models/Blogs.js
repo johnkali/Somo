d125443c75aa54ef6252bfc9eb7edc4d4719e075
johnkali <jdkali96@gmail.com>
2026-01-29 15:46:13 +0300

    Add Blogs schema rules for data

---
+++ server/models/Blogs.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+const blogSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            default: '',
+        },
+        author: {
+            type: String,
+            required: true,
+            ref: 'User' //link to users collection
+        },
+    },
+    { timestamps: true } //add createdAt & UpdatedAt
+);
+const Blog = mongoose.model('Blog', blogSchema);
+export default Blog;
