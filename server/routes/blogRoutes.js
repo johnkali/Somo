c96373e753398a53078f99a87cb337b9f759d23e
johnkali <jdkali96@gmail.com>
2026-02-04 11:45:03 +0300

    Add save favorite blog route

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -47,15 +47,50 @@ router.get("/:id", protect, async (req, res) => {
     try {
         const blog = await  Blog.findById(req.params.id);
         if (!blog) {
-            return res.status(404).send("No blog found!");
+            return res.status(404).json("No blog found!");
         }
         res.status(200).json(blog);
 
     }catch (error){
         console.error("FETCH BLOG ERROR",error);
-        res.status(500).send({message: "Server Error"});
+        res.status(500).json({message: "Server Error"});
     }
 
+})
+
+//Toggle save/unsave blog
+router.post("/:id/save", protect, async (req, res) => {
+    try {
+        const blogId = req.params.id;
+        const userId = req.user._id;
+
+        const user  =  await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).send("No user found!");
+        }
+
+        const isSaved = user.favorites.includes(blogId);
+
+        if (isSaved) {
+            //unsave
+            user.favorites =  user.favorites.filter(
+                (id) => id.toString() !== blogId);
+        }else {
+            //save
+            user.favorites.push(blogId);
+        }
+
+        await user.save();
+
+        res.json({
+            message: isSaved ? "Blog Unsaved!" : "Blog Saved!",
+            favorites: user.favorites,
+        });
+    }catch (error) {
+        console.error("SAVE BLOG ERROR",error);
+        res.status(500).json({message: "Server Error"});
+    }
 })
 export default router;
 
