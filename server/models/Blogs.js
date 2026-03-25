ca0943acc43e2ba3c7c462f639535499a0384733
John <jdkali96@gmail.com>
2026-03-25 12:13:07 +0300

    Add comments to Blogs schema

--- server/models/Blogs.js
+++ server/models/Blogs.js
@@ -1,26 +1,39 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 const blogSchema = new mongoose.Schema(
-    {
-        title: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        content: {
-            type: String,
-            required: true,
-        },
-        image: {
-            type: String,
-            default: '',
-        },
-        author: {
-            type: mongoose.Schema.Types.ObjectId,
-            required: true,
-            ref: 'Users' //link to users collection
-        },
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    { timestamps: true } //add createdAt & UpdatedAt
+    content: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Users", //link to users collection
+    },
+    comments: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        text: String,
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  { timestamps: true }, //add createdAt & UpdatedAt
 );
-const Blog = mongoose.model('Blogs', blogSchema);
+const Blog = mongoose.model("Blogs", blogSchema);
 export default Blog;
